cf0370cdd84c14715a53de2e7602cb81fa8fc75f
Suchitra Naik <susanaik@gmail.com>
2018-07-19 01:20:01 +0000

    automatically backed up by learn

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -1,6 +1,18 @@
 class CouponsController < ApplicationController
 
   def index
+    @coupons = Coupon.all
   end
+
+  def new
+  end 
+
+  def show 
+    @coupon = Coupon.find(params[:id])
+  end 
+  
+  def create
+    @coupon = Coupon.create(coupon_code: params[:coupon_code], store:  params[:store])
+  end 
 
 end
